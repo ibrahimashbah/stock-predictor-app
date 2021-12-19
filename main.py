8f3ca5507d91ebc26b2233490f112276151a2f04
ibrahimashbah <96091453+ibrahimashbah@users.noreply.github.com>
2021-12-19 01:46:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,14 @@ st.set_page_config(
     layout="wide",
    
 )
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
 col0 = st.columns(1)
 
